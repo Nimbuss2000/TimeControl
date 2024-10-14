b52cb0648679056113956a8391ef692babdae9fc
AL <alexl5105@mail.ru>
2024-10-14 20:19:20 +0300

    Initial Commit

---
+++ TimeControl/TimeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeControlApp.swift
+//  TimeControl
+//
+//  Created by Alexey Larionov on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeControlTests/TimeControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeControlTests.swift
+//  TimeControlTests
+//
+//  Created by Alexey Larionov on 14.10.2024.
+//
+
+import Testing
+@testable import TimeControl
+
+struct TimeControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
