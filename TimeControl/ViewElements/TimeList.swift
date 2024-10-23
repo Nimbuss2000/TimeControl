30132cd7b5f511f9735bb306a3f8a3a45dee6748
AL <alexl5105@mail.ru>
2024-10-24 02:54:33 +0300

    add worker timer

---
+++ TimeControl/ViewElements/TimeList.swift
@@ -0,0 +1,18 @@
+//
+//  TimwList.swift
+//  TimeControl
+//
+//  Created by Alexey Larionov on 20.10.2024.
+//
+
+import SwiftUI
+
+struct TimeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeList()
+}
